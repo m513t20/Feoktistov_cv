e0353db33b9421c284064d79c4e4ac96e9fca2cf
m513t20 <email431@email431.com>
2024-04-25 20:00:51 +0800

    переделал вывод под задание

--- figures_and_colors/main.py
+++ figures_and_colors/main.py
@@ -21,37 +21,17 @@ def get_shapes(regions):
         eccent=region.eccentricity
         if eccent==0:
             if (region.image.size==region.area):
-                key='sqare'
+                key='rectangle'
             else:
                 key='circle'
         else:
             key='rectangle'
 
-        # plt.clf()
-        # plt.title(f"symbol={key}")
-        # plt.imshow(region.image)
-        # plt.tight_layout()
-        # plt.savefig(Path(__file__).parent/"figs"/f"{index}.png")
 
         shapes[key]+=1
 
     return shapes
 
-
-def get_colors(img):
-   
-    colors=[]
-
-    for cur_val in np.unique(img):
-        tmp_img=np.copy(img)
-        tmp_img[tmp_img!=cur_val]=0
-        tmp_img[tmp_img>0]=1
-
-        labeled=label(tmp_img)
-
-        m_col=labeled.max()
-        colors.append(m_col)
-    return colors
 
 file=Path(__file__).parent/"balls_and_rects.png"
 
@@ -63,42 +43,37 @@ image=plt.imread(file)
 bin_image=np.mean(image,2)
 bin_image[bin_image>0]=1
 labeled=label(bin_image)
-regions=regionprops(labeled)
-
-
 
 
 print(f'overall figures={labeled.max()}')
-
-shapes=get_shapes(regions)
-for cur_key in shapes:
-    print(f'figures shaped like {cur_key} = {shapes[cur_key]}')
-
 
 
 
 hsv_image=rgb2hsv(image)
 
 
-unique_colors=np.unique(hsv_image[:,:,0])
-
+c=0
 prev=0
 ars=[[],[],[],[],[],[],[],[],[],[]]
 for index,i in enumerate(np.linspace(0,1,10)):
-    for cur in unique_colors:
-        if cur>prev and cur<i:
-            ars[index].append(cur)
+    tmp_image=np.copy(hsv_image)
+    tmp_image[tmp_image[:,:,0]<prev]=0
+    tmp_image[tmp_image[:,:,0]>i]=0
+    tmp_image[tmp_image[:,:,0]>0]=1
+
+    tmp_image=np.mean(tmp_image,2)
+
+    labeled=label(tmp_image)
+    if labeled.max()>0:
+        c+=1
+        print(f'color № {c}')
+        regions=regionprops(labeled)
+        shapes=get_shapes(regions)
+        for cur_key in shapes:
+            print(f'\tfigures shaped like {cur_key} = {shapes[cur_key]}')
+
+
+
     prev=i
 
-# print(ars)
 
-
-# plt.plot(unique_colors,"o")
-
-# plt.show()
-num=1
-for cur_step in ars:
-    if len (cur_step)>0:
-        print(f'color {num} = {len(cur_step)}')
-        num+=1
-print('and 1 object colored black')
